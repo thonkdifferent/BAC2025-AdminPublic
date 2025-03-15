0cb2eb475265b426fad1f39b1eecb11a4845bbae
Al-Tex <94311415+Al-Tex@users.noreply.github.com>
2025-03-15 19:13:06 +0200

    Add map popup

--- lib/tabs/MapTab.dart
+++ lib/tabs/MapTab.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 import 'package:latlong2/latlong.dart';
+import '../widgets/ParkingMap.dart';
 import '../widgets/PopupMenu.dart';
 class MapTab extends StatefulWidget{
 
@@ -15,28 +16,7 @@ class _MapTabState extends State{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(actions:<Widget>[PopupMenu()]),
-      body:FlutterMap(
-        options: MapOptions(
-          initialCenter: LatLng(51.509364, -0.128928), // Center the map over London
-          initialZoom: 9.2,
-        ),
-        children: [
-          TileLayer( // Bring your own tiles
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
-            userAgentPackageName: 'com.itfest25.carspace', // Add your app identifier
-            // And many more recommended properties!
-          ),
-          RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
-                onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
-              ),
-              // Also add images...
-            ],
-          ),
-        ],
-      )
+      body: ParkingMap()
     );
   }
 }

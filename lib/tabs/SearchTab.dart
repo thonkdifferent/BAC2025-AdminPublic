288e9a4389d0560d1a56ca3617abcc6f94c29e4b
KOZTY <95423121+KOZTY@users.noreply.github.com>
2025-03-16 15:02:06 +0200

    Added Search Bar

--- lib/tabs/SearchTab.dart
+++ lib/tabs/SearchTab.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
+class SearchTab extends StatefulWidget {
+  const SearchTab({super.key});
 
+  @override
+  State<SearchTab> createState() => _SearchTabState();
+}
 
-class SearchTab extends StatelessWidget{
+class _SearchTabState extends State<SearchTab> {
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("search here")
+        child: Scaffold(
+        appBar: AppBar(title: const Text('Search Location')),
+        body: Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: SearchAnchor(
+            builder: (BuildContext context, SearchController controller) {
+              return SearchBar(
+                controller: controller,
+                padding: const WidgetStatePropertyAll<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+                onTap: () {
+                  controller.openView();
+                },
+                onChanged: (_) {
+                  controller.openView();
+                },
+              );
+            },
+            suggestionsBuilder: (BuildContext context, SearchController controller) {
+              return List<ListTile>.generate(0, (int index) {
+                final String item = 'item $index';
+                return ListTile(
+                  title: Text(item),
+                  onTap: () {
+                    setState(() {
+                      controller.closeView(item);
+                    });
+                  },
+                );
+              });
+            },
+          ),
+        ),
+      )
     );
   }
 }

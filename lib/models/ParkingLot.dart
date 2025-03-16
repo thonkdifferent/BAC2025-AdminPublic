30e67a78113c4c6bd400064b507bbd92a0a02d94
Al-Tex <94311415+Al-Tex@users.noreply.github.com>
2025-03-16 17:03:15 +0200

    added parking lot

---
+++ lib/models/ParkingLot.dart
@@ -0,0 +1,9 @@
+import 'ParkingLot.dart';
+
+class ParkingLot{
+  final int places;
+  final int availablePlaces;
+  final int occupiedPlaces;
+
+  ParkingLot({required this.places,required this.availablePlaces,required this.occupiedPlaces});
+}
